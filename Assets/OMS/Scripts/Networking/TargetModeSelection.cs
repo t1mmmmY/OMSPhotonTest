aa0eaede5e63220eadbbb66b972aef94a2285411
Denis Dovganich <t1mmmmY111@gmail.com>
2018-10-26 14:46:34 +0200

    Network lobby for VR and standalone

---
+++ Assets/OMS/Scripts/Networking/TargetModeSelection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetModeSelection : MonoBehaviour 
+{
+    [SerializeField] GameObject[] vrObjects;
+    [SerializeField] MonoBehaviour[] vrComponents;
+
+    [SerializeField] GameObject[] standaloneObjects;
+    [SerializeField] MonoBehaviour[] standaloneComponents;
+
+    void Start()
+    {
+        SetMode(UnityEngine.XR.XRDevice.isPresent);
+    }
+
+    void SetMode(bool isVR)
+    {
+        foreach (GameObject go in vrObjects)
+        {
+            go.SetActive(isVR);
+        }
+
+        foreach (MonoBehaviour component in vrComponents)
+        {
+            component.enabled = isVR;
+        }
+
+        foreach (GameObject go in standaloneObjects)
+        {
+            go.SetActive(!isVR);
+        }
+
+        foreach (MonoBehaviour component in standaloneComponents)
+        {
+            component.enabled = !isVR;
+        }
+    }
+}
